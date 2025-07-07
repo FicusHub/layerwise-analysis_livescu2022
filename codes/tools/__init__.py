1cb24a3a2835ac10f151e2709d19353b11b40231
Sandra Arcos Holzinger <sarcosh1@jh.edu>
2025-07-07 16:51:51 -0400

    Add get_cca_score to module exports

--- codes/tools/__init__.py
+++ codes/tools/__init__.py
@@ -1,4 +1,4 @@
 # Make tools directory a Python package
 from .tools_utils import LAYER_CNT
 from .cca_core import CCA
-from .get_scores import evaluate_cca 
+from .get_scores import evaluate_cca, get_cca_score 
